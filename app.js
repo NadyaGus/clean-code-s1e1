e4a7f79bc3daf08e4166c4ab427623664c3f4123
NadyaGus <ftotkexit@gmail.com>
2024-01-08 12:05:33 +0300

    fix(extended-1.2): fix alt for new images in JS file

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete-img";
+    deleteButtonImg.alt="Remove";
     deleteButton.appendChild(deleteButtonImg);
 
 
